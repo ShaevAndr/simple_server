398ceea4644de959f3416b1b3e9a879f7229925f
ShaevAndrey <spinoza33@yandex.ru>
2022-11-01 23:06:06 +0300

    first commit

---
+++ index.js
@@ -0,0 +1,32 @@
+const express = require('express'),
+    Twig = require("twig");
+
+
+const app = express()
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+app.set("twig options", {
+    allow_async: true, // Allow asynchronous compiling
+    strict_variables: false
+});
+
+
+
+app.get("/", (req, res) =>{
+    console.log('get data')
+    res.render('control_settings.twig', {
+        select_menager:{
+            items  : [{id:1, option:"Выбор пользователя / отдела", additional_data:"fergfds"}, {id:2, option:"test1"}, {id:3, option:"test4"}, {id:4, option:"test3"},], 
+            id: "select_manager"
+        },
+        delay_1:{
+            name: "delay_1",
+            id: "delay_1",
+            placeholder: "Кол-во мин. без ответа клиенту"
+        },
+    });
+})
+
+
+app.listen(2000, () => console.log("app is starting")); 
---
+++ test.js
@@ -0,0 +1,6 @@
+const obj = {abs:{df:"sdf"}, abc:{dfe:"sdf"}, abd:{dfer:"sdf"}, }
+
+obj.map(el=>{console.log(el)})
+// for (const i in obj) {
+//     console.log(obj[i])
+// }
